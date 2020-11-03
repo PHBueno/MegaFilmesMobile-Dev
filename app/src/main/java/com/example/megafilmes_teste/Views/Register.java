d4af3bb0932793729991912bbaaa1fbb6b5a1016
PHBueno <contato0113@gmail.com>
2020-11-03 19:21:24 -0300

    Informações de usuários sendo guardadas no Firebase

--- app/src/main/java/com/example/megafilmes_teste/Views/Register.java
+++ app/src/main/java/com/example/megafilmes_teste/Views/Register.java
@@ -3,10 +3,10 @@ package com.example.megafilmes_teste.Views;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,16 +16,26 @@ import android.widget.Toast;
 
 import com.example.megafilmes_teste.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
-     private Button button;
-     private EditText email, username, password;
-     private TextView login;
-     private FirebaseAuth fAuth;
-     private ProgressBar ProgressBar;
+    public static final String TAG = "TAG";
+    private Button button;
+    private EditText email, firstname, lastname, password;
+    private TextView login;
+    private FirebaseAuth fAuth;
+    private FirebaseFirestore fStore;
+    private ProgressBar ProgressBar;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +43,14 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         email = findViewById(R.id.newEmail);
-        username = findViewById(R.id.newUser);
+        firstname = findViewById(R.id.newFirstName);
+        lastname = findViewById(R.id.newLastName);
         password = findViewById(R.id.editTextTextPassword);
         ProgressBar = findViewById(R.id.progressBar);
         login = findViewById(R.id.ComeLogin);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         button = (Button) findViewById(R.id.NewRegister);
 
@@ -47,6 +59,8 @@ public class Register extends AppCompatActivity {
             public void onClick(View v) {
                 String NewEmail = email.getText().toString().trim();
                 String NewPasswd = password.getText().toString().trim();
+                final String fname = firstname.getText().toString();
+                final String lname = lastname.getText().toString();
 
                 if(TextUtils.isEmpty(NewEmail)){
                     email.setError("The Email is required!");
@@ -67,7 +81,18 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this, "User Created", Toast.LENGTH_LONG).show();
-                            GoMovies();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("FirstName", fname);
+                            user.put("LastName", lname);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "User profile was created");
+                                }
+                            });
+                            GoLogin();
                         }else {
                             Toast.makeText(Register.this, "Error " + task.getException().getMessage(),Toast.LENGTH_LONG).show();
                         }
@@ -83,10 +108,10 @@ public class Register extends AppCompatActivity {
         });
     }
 
-    public void GoMovies(){
+    /*public void GoMovies(){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
-    }
+    }*/
 
     public void GoLogin(){
         Intent intent = new Intent(this, Login.class);
